374a614e142e1cc0828c60f0998b31a78c3a5e1f
mdcone <mdcone@gmail.com>
2013-04-29 17:43:15 -0700

    Update yandex.rb
    Start fresh

--- lib/geocoder/results/yandex.rb
+++ lib/geocoder/results/yandex.rb
@@ -71,7 +71,7 @@ module Geocoder::Result
 
     def sub_state_city
       if sub_state && sub_state["Locality"]
-        sub_state['Locality']['DependentLocality']['DependentLocalityName']      
+        sub_state['Locality']['LocalityName']      
       else
         ""
       end
